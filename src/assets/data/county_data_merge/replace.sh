dd5ba65c3ad1e5c0071bee6dec07c57fc7fe90b7
sam <sbbaumo@yahoo.com>
2021-03-03 23:10:34 -0600

    added state data

--- src/assets/data/county_data_merge/replace.sh
+++ src/assets/data/county_data_merge/replace.sh
@@ -1,8 +1,13 @@
 ogr2ogr -f GeoJSON ../shapefiles/output.json ../shapefiles/cb_2019_us_county_20m.shp
 python counties.py
 geo2topo counties=final.json > topoout.json
+
 cp topoout.json ../2019_county_election_map_topo.json
 cp final.json ../2019_county_election_map_geo.json
+
+rm ./final.json
+rm ./topoout.json
+rm ../shapefiles/output.json
 
 # ogr2ogr -simplify 0.0040 iter.shp cb_2019_us_county_500k.shp
 # ogr2ogr -f GeoJSON output.json input.shp
